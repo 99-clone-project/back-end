f253ce0e4956bb564ce10c3afc069848170c178b
jhhong0930 <jhhong0930@gmail.com>
2021-10-21 09:36:18 +0900

    add: 비밀번호 유효성 체크 추가

--- src/main/java/com/sparta/backend/controller/UserController.java
+++ src/main/java/com/sparta/backend/controller/UserController.java
@@ -3,11 +3,13 @@ package com.sparta.backend.controller;
 import com.sparta.backend.dto.SignupRequestDto;
 import com.sparta.backend.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.validation.Errors;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
+import javax.validation.Valid;
 import java.util.List;
 import java.util.Map;
 
@@ -24,17 +26,22 @@ public class UserController {
 
     // 회원가입 요청
     @PostMapping("/user/signup")
-    public String  registerUser(@RequestBody SignupRequestDto requestDto) {
+    public String  registerUser(@Valid @RequestBody SignupRequestDto requestDto, Errors errors) {
+
+        if(errors.hasErrors()){
+            return "redirect:/user/signup";
+        }
+
         userService.registerUser(requestDto);
         return "redirect:/user/login";
     }
 
-    //회원 가입 페이지
+    // 회원 가입 페이지
     @GetMapping("/user/signup")
     public String signup() {return "signup";}
 
 
-    //로그인 요청
+    // 로그인 요청
     @PostMapping("/user/login")
     public List<Map<String, String>> login(@RequestBody SignupRequestDto requestDto) {
         List<Map<String, String>> ret = userService.login(requestDto);
